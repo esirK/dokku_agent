37a52db5ebfd63fa34d7d0e550d942d87da11904
esir <esirkings@gmail.com>
2022-09-28 12:03:31 +0300

    Get app details via an endpoint

--- main.go
+++ main.go
@@ -17,9 +17,22 @@ func main() {
 	http.HandleFunc("/apps", getApps)
 	http.HandleFunc("/apps/create", createApp)
 	http.HandleFunc("/apps/destroy", destroyApp)
+	http.HandleFunc("/apps/details", getAppDetails)
 
 	fmt.Println("Server is running on port 8080")
 	http.ListenAndServe(":8080", nil)
+}
+
+func parseDokkuOutput(output string) map[string]string {
+	lines := strings.Split(strings.TrimSpace(output), "\n")
+	result := make(map[string]string)
+	for _, line := range lines {
+		parts := strings.Split(line, ":")
+		if len(parts) == 2 {
+			result[strings.TrimSpace(parts[0])] = strings.TrimSpace(parts[1])
+		}
+	}
+	return result
 }
 
 func getApps(w http.ResponseWriter, r *http.Request) {
@@ -38,16 +51,10 @@ func getApps(w http.ResponseWriter, r *http.Request) {
 			fmt.Println("Error: ", err)
 		}
 		// Parse app report
-		lines := strings.Split(strings.TrimSpace(out), "\n")
-		appReport := make(map[string]string)
-		for _, line := range lines {
-			parts := strings.Split(line, ":")
-			if len(parts) == 2 {
-				appReport[strings.TrimSpace(parts[0])] = strings.TrimSpace(parts[1])
-			}
-		}
-		timestamp, err := strconv.ParseInt(appReport["Created at"], 0, 64)
-		
+		appReport := parseDokkuOutput(out)
+
+		timestamp, err := strconv.ParseInt(appReport["App created at"], 0, 64)
+
 		if err != nil {
 			fmt.Println("Error: ", err)
 		}
@@ -55,11 +62,11 @@ func getApps(w http.ResponseWriter, r *http.Request) {
 		// Create app object
 		resp["apps"] = append(resp["apps"], models.DokkuApp{
 			Name:      app,
-			GitUrl:    "https://github.com/"+app,
+			GitUrl:    "https://github.com/" + app,
 			GitBranch: "master",
 			CreatedAt: createdAt.Format(time.RFC1123),
 			Status:    "running",
-			Details: appReport,
+			Details:   appReport,
 		})
 	}
 
@@ -83,6 +90,65 @@ func createApp(w http.ResponseWriter, r *http.Request) {
 	}
 	// Return 204 No Content
 	w.WriteHeader(http.StatusNoContent)
+}
+
+func buildOutput(out string) map[string]string {
+	result := make(map[string]string)
+	for key, value := range parseDokkuOutput(out) {
+		result[key] = value
+	}
+	return result
+}
+
+func getAppDetails(w http.ResponseWriter, r *http.Request) {
+	// Read app name from request
+	appName := r.URL.Query().Get("name")
+
+	// Get app report
+	report, err := runCommand("dokku", "apps:report", appName)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	appReport := buildOutput(report)
+
+	// Get app config
+	config, err := runCommand("dokku", "config:show", appName)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	configArray := buildOutput(config)
+
+	// Get app domains
+	domains, err := runCommand("dokku", "domains:report", appName)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	appDomains := buildOutput(domains)
+
+	response := make(map[string]models.DokkuAppDetails)
+	response[appName] = models.DokkuAppDetails{
+		Config: configArray,
+		Domain: models.Domain{
+			Enabled:      appDomains["Domains app enabled"] == "true",
+			AppVhosts:    strings.Split(appDomains["Domains app vhosts"], " "),
+			GlobalVhosts: strings.Split(appDomains["Domains global vhosts"], " "),
+		},
+		Report: models.Report{
+			Dir:    appReport["App dir"],
+			Locked: appReport["App locked"] == "true",
+		},
+	}
+
+	jData, err := json.Marshal(response)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Write(jData)
 }
 
 func destroyApp(w http.ResponseWriter, r *http.Request) {
--- models/app.go
+++ models/app.go
@@ -1,10 +1,27 @@
 package models
 
 type DokkuApp struct {
-	  Name string
-	  GitUrl string
-	  GitBranch string
-	  CreatedAt string
-	  Status string
-	  Details map[string]string
+	Name      string
+	GitUrl    string
+	GitBranch string
+	CreatedAt string
+	Status    string
+	Details   map[string]string
+}
+
+type DokkuAppDetails struct {
+	Config map[string]string
+	Domain Domain
+	Report Report
+}
+
+type Domain struct {
+	Enabled      bool
+	AppVhosts    []string
+	GlobalVhosts []string
+}
+
+type Report struct {
+	Dir    string
+	Locked bool
 }
